1381abecb1d5a3e023d2e944457a722825c799b6
VHSVoyager <paulfalkenberg23@gmail.com>
2026-01-28 18:43:36 +0000

    Initial Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,65 +1,68 @@
 import altair as alt
 import pandas as pd
 import streamlit as st
+from datetime import timedelta
 
 # Show the page title and description.
-st.set_page_config(page_title="Movies dataset", page_icon="🎬")
-st.title("🎬 Movies dataset")
+st.set_page_config(page_title="Teater Totters", page_icon="🐮")
+st.title("🐮 Teater Totters")
 st.write(
     """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
-    It shows which movie genre performed best at the box office over the years. Just 
-    click on the widgets below to explore!
+    Guild Data Visualizer!
     """
 )
-
 
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/movies_genres_summary.csv")
+    df = pd.read_csv("data/tt_data.csv")
+    df['Date'] = pd.to_datetime(df['Date'])
     return df
 
 
 df = load_data()
 
-# Show a multiselect widget with the genres using `st.multiselect`.
-genres = st.multiselect(
-    "Genres",
-    df.genre.unique(),
-    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
+# Show a multiselect widget with the Names using `st.multiselect`.
+members = st.multiselect(
+    "Members",
+    df.Name.unique(),
+    [],
 )
 
-# Show a slider widget with the years using `st.slider`.
-years = st.slider("Years", 1986, 2006, (2000, 2016))
+# Show a slider widget with the Dates using `st.slider`.
+dates = st.slider("Date", 
+                  df['Date'].min().to_pydatetime(), 
+                  df['Date'].max().to_pydatetime(), 
+                 (df['Date'].min().to_pydatetime(), df['Date'].max().to_pydatetime()),
+                 step=timedelta(days=7))
 
 # Filter the dataframe based on the widget input and reshape it.
-df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
+df_filtered = df[(df["Name"].isin(members)) & (df["Date"].between(dates[0], dates[1]))]
 df_reshaped = df_filtered.pivot_table(
-    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
+    index="Date", columns="Name", values="Experience", aggfunc="sum", fill_value=0
 )
-df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
+df_reshaped = df_reshaped.sort_values(by="Date", ascending=True)
 
 
 # Display the data as a table using `st.dataframe`.
 st.dataframe(
     df_reshaped,
     use_container_width=True,
-    column_config={"year": st.column_config.TextColumn("Year")},
+    column_config={"Date": st.column_config.DatetimeColumn("Date", format='MMM D, YYYY', width='auto')},
 )
 
 # Display the data as an Altair chart using `st.altair_chart`.
 df_chart = pd.melt(
-    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
+    df_reshaped.reset_index(), id_vars="Date", var_name="Name", value_name="gross"
 )
 chart = (
     alt.Chart(df_chart)
     .mark_line()
     .encode(
-        x=alt.X("year:N", title="Year"),
-        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-        color="genre:N",
+        x=alt.X("Date:T", title="Date"),
+        y=alt.Y("gross:Q", title="Total Experience"),
+        color="Name:N",
     )
     .properties(height=320)
 )
